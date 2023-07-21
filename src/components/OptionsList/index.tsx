9e1eefe338f4055c223234c23e39d182b9b44652
Andre Cabrera <cabrerandre@gmail.com>
2023-07-21 14:03:18 -0400

    testing with single class

--- src/components/OptionsList/index.tsx
+++ src/components/OptionsList/index.tsx
@@ -1,5 +1,6 @@
 import { Options } from '@types';
 import Option from '@components/Option';
+import styles from '@styles/LandingPage.module.scss';
 
 interface OptionsListProps {
   className: string;
@@ -8,7 +9,7 @@ interface OptionsListProps {
 
 const OptionsList = ({ className, options }: OptionsListProps) => {
   return (
-    <div className={className}>
+    <div className={styles.landing_page}>
       {options.map((option) => {
         return <Option key={option.label} option={option} />;
       })}
