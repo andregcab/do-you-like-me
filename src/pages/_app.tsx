3703f0c4b0e4705e7496cf399477d82680801f2a
Andre Cabrera <cabrerandre@gmail.com>
2023-07-21 13:53:51 -0400

    reintroduce transition wrapper for test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import AppContextProvider from '@context';
-// import TransitionWrapper from '@components/TransitionWrapper';
+import TransitionWrapper from '@components/TransitionWrapper';
 import NotebookPaper from '@components/NotebookPaper';
 import '@styles/global.scss';
 
@@ -9,11 +9,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <AppContextProvider>
-        {/* <TransitionWrapper> */}
-        <NotebookPaper>
-          <Component {...pageProps} />;
-        </NotebookPaper>
-        {/* </TransitionWrapper> */}
+        <TransitionWrapper>
+          <NotebookPaper>
+            <Component {...pageProps} />;
+          </NotebookPaper>
+        </TransitionWrapper>
       </AppContextProvider>
     </ChakraProvider>
   );
